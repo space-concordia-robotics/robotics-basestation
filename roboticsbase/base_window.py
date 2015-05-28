70d12e6f7b6002135a78fb354c4d95d32632ce83
doomfest <gonis.george@gmail.com>
2015-05-28 00:00:14 -0400

    Added vertical boxes for structure and closing the window displays a messagebox.

---
+++ roboticsbase/base_window.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python2.7
+
+from common_headers import *
+
+class BaseWindow:
+    # Create the base window where all other items will be.
+    def __init__(self):
+        self.app_window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        self.app_window.set_size_request(WINDOW_WIDTH, WINDOW_HEIGHT)
+        self.app_window.set_border_width(WINDOW_BORDER_WIDTH)
+        self.app_window.set_title(WINDOW_TITLE)
+        self.app_window.set_position(gtk.WIN_POS_CENTER)
+
+        ############################
+        # Video Box
+        ############################
+        self.video_box = gtk.HBox()
+        
+        ############################
+        # Status Box
+        ############################
+        self.status_box = gtk.HBox()
+
+        ############################
+        # Bottom Widgets
+        ############################
+        
+        # Exit button
+        self.exit_button = gtk.Button("Exit")
+     #  exit_button.connect("clicked", self.delete_event)
+        self.widget_box = gtk.HBox()
+        self.widget_box.pack_start(self.exit_button)
+
+        # Put all box into the main one.
+        self.main_box = gtk.VBox()
+        self.main_box.pack_start(self.video_box)
+        self.main_box.pack_start(self.status_box)
+        self.main_box.pack_start(self.widget_box)
+
+        # Show everything in window
+        self.app_window.add(self.main_box)
+        self.app_window.show_all()
+
+        # Used when closing window.
+        self.app_window.connect("delete_event", self.delete_event)
+        self.app_window.connect("destroy", self.destroy)
+
+    def delete_event(self, widget, event, data=None):
+        msg = "Are you sure you want to quit?"
+        md = gtk.MessageDialog(self.app_window,
+                               gtk.DIALOG_DESTROY_WITH_PARENT,
+                               gtk.MESSAGE_QUESTION,
+                               gtk.BUTTONS_YES_NO,
+                               msg)
+        md.set_title("Quit the program?")
+
+        response = md.run()
+
+        if response == gtk.RESPONSE_YES:
+            md.destroy()
+            self.destroy(self, widget)
+            return False
+        else:
+            md.destroy()
+            return True
+
+    def destroy(self, widget, data=None):
+        gtk.main_quit()
+   
+    def main(self):
+        gtk.main()
+
+# Create base window and all its elements and run it.
+if __name__ == "__main__":
+    base = BaseWindow()
+    base.main()
+
---
+++ roboticsbase/common_headers.py
@@ -0,0 +1,9 @@
+import pygtk
+pygtk.require('2.0')
+import gtk
+
+WINDOW_WIDTH = 1024
+WINDOW_HEIGHT = 600
+WINDOW_BORDER_WIDTH = 10
+WINDOW_TITLE = "BaseStation - Rover"
+
