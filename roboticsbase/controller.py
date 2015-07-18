b9ac661ecacaff30659509c1c3291d9c1ea24a4d
msnidal <mark.snidal@gmail.com>
2015-07-18 15:10:14 -0400

    Unified station-side client code with a file and method

--- roboticsbase/controller.py
+++ roboticsbase/controller.py
@@ -1,17 +1,18 @@
-#!/usr/bin/env python3
-
 import pygame
 import time
 import threading
-from roboticsnet.client import rover_client
 
-JOYSTICK_ID = 0
+from common_constants import *
+from roboticsnet.gateway_constants import *
+from profiles.logitech_F310 import *
+
+from station_client import send_command
 
 def init_gamepad():    
     if (pygame.joystick.get_count() > 0):
         joystick = pygame.joystick.Joystick(JOYSTICK_ID)
         joystick.init()
-        print "Joystick active on slot %d" % JOYSTICK_ID
+        print "Joystick active on slot %d. Using %s profile" % (JOYSTICK_ID, JOYSTICK_NAME)
         return joystick
     else:
         print "Error - no joystick detected."
@@ -19,62 +20,56 @@ def init_gamepad():
 
 def get_joystick_value(joystick):
     # temp mapping values - waiting on finalized hook values
-    x = joystick.get_axis(3)*128
-    y = (-joystick.get_axis(4))*128
+    x = int(joystick.get_axis(AXIS_RSTICK_X)*128+128)
+    y = int((-joystick.get_axis(AXIS_RSTICK_Y))*128+128)
     
     return (x,y)
 
-def joystick_listener(joystick):
-    exit = False
-    client = rover_client.RoverClient() #host, port temporarily set to default values as per roboticsnet constants. Pass these in from the base station interface?
-    print "New client established using host %s and port %d" % (client.getHost(), client.getPort())
-     
-    while exit == False:
+def joystick_listener(joystick, host, port, events):    
+    while events[STOP_CONTROLLER_EVENT].is_set() == False:
         # Button logic
         for event in pygame.event.get():
             if event.type == pygame.JOYBUTTONDOWN:
-                if event.joy == JOYSTICK_ID and event.button == 0:
-                    exit = True
-                #elif...
+                if event.joy == JOYSTICK_ID and event.button == BUTTON_A:
+                    events[STOP_CONTROLLER_EVENT].set()
+                elif event.joy == JOYSTICK_ID and event.button == BUTTON_B:
+                    events[VID_STREAM_EVENT].set()
+                elif event.joy == JOYSTICK_ID and event.button == BUTTON_X:
+                    events[VID_STOP_EVENT].set()
+                # elif...
         
         # Joystick logic
         (x,y) = get_joystick_value(joystick)
         
         print "X: %d\nY: %d" % (x,y)
         
-        if x < -30:
+        if x < (128-30):
             print "left %d" % x
-            #leftMotorValue = 96
-            #rightMotorValue = 224
-            #client.turnLeft(x)
+            send_command(ROBOTICSNET_COMMAND_TURNLEFT, x, host, port)
             
-        elif x > 30:
+        elif x > (128+30):
             print "right %d" % x
-            #leftMotorValue = 32
-            #rightMotorValue = 160
-            #client.turnRight(x)
+            send_command(ROBOTICSNET_COMMAND_TURNRIGHT, x, host, port)
             
-        elif y > 10:
-            #y1 = int(193-y)
-            #y2 = int(65-y)
+        elif y > (128+10):
             print "forward %d" % y
-            #client.forward(y)
+            send_command(ROBOTICSNET_COMMAND_FORWARD, x, host, port)
     
-        elif y < -10:
-            #y1 = int(193 + y)
-            #y2 = int(65 + y)
+        elif y < (128-10):
             print "reverse %d" % y
-            #client.forward(y)
+            send_command(ROBOTICSNET_COMMAND_REVERSE, x, host, port)
                 
         else:
             print "stop"
-            #client.stop(0)
+            send_command(ROBOTICSNET_COMMAND_STOP, 0, host, port)
         
-        time.sleep(0.125)
-        
+        time.sleep(0.25)
+    
+    # send one final stop command
+    send_command(ROBOTICSNET_COMMAND_STOP, 0, host, port)
     pygame.quit()
     
-def spawn_joystick_thread():
+def spawn_joystick_thread(host, port, events):
     pygame.init()
     joystick = init_gamepad()
     if (joystick == 0):
@@ -82,11 +77,12 @@ def spawn_joystick_thread():
         pygame.quit()
     else:
         print "Starting joystick listener thread..."
-        t = threading.Thread(target=joystick_listener(joystick), args=())
+        t = threading.Thread(target=joystick_listener, args=(joystick, host, port, events))
         t.start()
 
 def main():
-    spawn_joystick_thread()
+    e = [threading.Event() for i in range(NUM_BUTTON_EVENTS)]
+    spawn_joystick_thread('localhost', 5000, e)
     
 main()
 
---
+++ roboticsbase/station_client.py
@@ -0,0 +1,37 @@
+from roboticsnet.client.rover_client import RoverClient
+from roboticsnet.gateway_constants import *
+from roboticsnet.roboticsnet_exception import RoboticsnetException
+
+def send_command(command, value=0, host='localhost', port=ROBOTICSNET_PORT):
+    client = RoverClient(host, port)
+    print "Using %s:%d:" % (client.getHost(), client.getPort())
+    
+    try:
+        if (command == ROBOTICSNET_COMMAND_GRACEFUL):
+            client.graceful()
+        elif (command == ROBOTICSNET_COMMAND_FORWARD):
+            client.forward(value)
+        elif (command == ROBOTICSNET_COMMAND_REVERSE):
+            client.reverse(value)
+        elif (command == ROBOTICSNET_COMMAND_TURNLEFT):
+            client.turnLeft(value)
+        elif (command == ROBOTICSNET_COMMAND_TURNRIGHT):
+            client.turnRight(value)
+        elif (command == ROBOTICSNET_COMMAND_STOP):
+            client.stop(value)
+        elif (command == ROBOTICSNET_COMMAND_QUERYPROC):
+            client.query()
+        elif (command == ROBOTICSNET_COMMAND_START_VID):
+            client.startVideo()
+        elif (command == ROBOTICSNET_COMMAND_STOP_VID):
+            client.stopVideo()
+            
+        print "Sent %d:%d." % (command, value)
+
+    except RoboticsnetException as e:
+        print "Station client failed!"
+        print e.message
+
+    except Exception as e:
+        print "Critical station client failure!"
+        print e.message
