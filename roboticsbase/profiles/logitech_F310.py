f87e41ba0f1b52fdc5cba37275cbd6e190169daa
msnidal <mark.snidal@gmail.com>
2015-07-18 03:04:09 -0400

    Added controller profiles

---
+++ roboticsbase/profiles/logitech_F310.py
@@ -0,0 +1,28 @@
+"""
+This profile contains a number of constants used to map a controller's axes and buttons to pygame's internal values.
+Use controller_tester.py to determine these values for any new controllers. This profile is for my Logitech gamepad at home.
+Keep associated documentation and test notes in /doc/controller_test_values.txt
+
+Author:
+    msnidal
+"""
+
+JOYSTICK_NAME = "Logitech F310"
+
+# Axes assignments
+AXIS_LSTICK_X = 0
+AXIS_LSTICK_Y = 1
+AXIS_LTRIG = 2
+AXIS_RSTICK_X = 3
+AXIS_RSTICK_Y = 4
+AXIS_RTRIG = 5
+
+# Button assignments 
+BUTTON_A = 0
+BUTTON_B = 1
+BUTTON_X = 2
+BUTTON_Y = 3
+BUTTON_LBUMPER = 4
+BUTTON_RBUMPER = 5
+BUTTON_SELECT = 6
+BUTTON_START = 7
