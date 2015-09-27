c8dc7f003aeba5c69e366297fa47900f63601080
psyomn <lethaljellybean@gmail.com>
2015-09-27 05:08:16 -0400

    .travis.yml: remove pygame version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import roboticsbase
 
 # requirements and links can be added and removed here. PyGTK does not support distutils on linux so it is not included for now.
 REQUIREMENTS = [
-    'pygame>=1.9'
+    'pygame'
 ]
 
 DEPENDENCY_LINKS = [
